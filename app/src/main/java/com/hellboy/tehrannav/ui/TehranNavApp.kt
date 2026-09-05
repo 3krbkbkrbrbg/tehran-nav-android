979ea578bbbcfba116c0c6cfaacba37dcc031ea0
3krbkbkrbrbg <3krbkbkrbrbg@users.noreply.github.com>
2026-09-05 14:17:59 +0330

    Fix search layout imports

--- app/src/main/java/com/hellboy/tehrannav/ui/TehranNavApp.kt
+++ app/src/main/java/com/hellboy/tehrannav/ui/TehranNavApp.kt
@@ -2,6 +2,7 @@ package com.hellboy.tehrannav.ui
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -9,6 +10,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
