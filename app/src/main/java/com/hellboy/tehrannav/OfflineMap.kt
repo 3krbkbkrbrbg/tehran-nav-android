cfbb828f76e23f90cdc3e6988ad322bb8bef6971
3krbkbkrbrbg <3krbkbkrbrbg@users.noreply.github.com>
2026-09-05 08:38:22 +0330

    Fix onTaskFailed: empty Unit body

--- app/src/main/java/com/hellboy/tehrannav/OfflineMap.kt
+++ app/src/main/java/com/hellboy/tehrannav/OfflineMap.kt
@@ -36,9 +36,7 @@ class OfflineMap(
                     object : CacheManager.CacheManagerCallback {
                         override fun downloadStarted() {}
                         override fun setPossibleTilesInArea(total: Int) {}
-                        override fun onTaskFailed() {
-                            listener?.onFinished(false, count)
-                        }
+                        override fun onTaskFailed() {}
                         override fun updateProgress(progress: Int, currentZoomLevel: Int, zoomMin: Int, zoomMax: Int) {
                             listener?.onProgress(progress, total)
                         }
